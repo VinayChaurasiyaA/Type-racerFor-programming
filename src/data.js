5cbe10371c486b8c7ee524da604f6f50060ed2bd
VINAY CHAURASIYA <vinaychaurasiya73@gmail.com>
2023-04-25 21:27:42 +0530

    adding data

--- src/data.js
+++ src/data.js
@@ -4,11 +4,16 @@ export const data = () =>
   );
 
 export const person = {
-  1:
-    "<!DOCTYPE html> <html> <head> <title> My Website </title> </head> <body> <h1> Welcome to my website </h1> <p> Here is some text. </p> <img src=myimage.jpg alt=My Image> </body> </html>".split(
-      " "
-    ),
-  2: "Doe mama nigga bitch".split(" "),
-  3: "happy birthday nigga have a splendours night".split(" "),
-  4: "blue sky makes us wet as well as dry".split(" "),
+  1: "<!DOCTYPE html> <html> <head> <title> My Website </title> </head> <body> <h1> Welcome to my website </h1> <p> Here is some text. </p> <img src=myimage.jpg alt=My Image> </body> </html>".split(
+    " "
+  ),
+  2: "public static void main( { } ) , private static void main( { } ) , abstract<newClass> super(); </newClass>".split(
+    " "
+  ),
+  3: "class SmallestInfiniteSet { public SmallestInfiniteSet() { } public int popSmallest() { } public void addBack(int num) { } }".split(
+    " "
+  ),
+  4: "#include <iostream> using namespace std; int main() { int num1, num2; cout << Enter the first number: ; cin >> num1; cout << Enter the second number: ; cin >> num2; int sum = num1 + num2; cout << The sum is:  << sum << endl; return 0; }".split(
+    " "
+  ),
 };
